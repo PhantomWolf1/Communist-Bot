468b257b42730234a059dacb32a03e73ddf34961
Austin Gallippo <phantomwolf154@gmail.com>
2020-01-05 12:59:02 -0500

    Create purge.js

---
+++ commands/purge.js
@@ -0,0 +1,11 @@
+module.exports.run = (bot, message, args, Discord) => {
+  if(message.author.hasPermission(["DELETE_MESSAGE"])) {
+    message.channel.bulkDelete(args[0]);
+  } else {
+    message.channel.say("❌ You do not have permission to use this ❌");
+  }
+}
+
+module.exports.help = {
+"name": "purge"
+}
